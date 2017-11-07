731b7be52532bbe8fb53d444b8660e47f4d2cf95
Vinícius Egidio <vegidio@gmail.com>
2017-11-07 10:20:07 +0100

    Updating Gradle and adding new Google repo

--- app/src/main/java/io/vinicius/androidcommon/util/RegexUtil.kt
+++ app/src/main/java/io/vinicius/androidcommon/util/RegexUtil.kt
@@ -5,47 +5,47 @@ class RegexUtil(pattern: String)
     companion object
     {
         /*
-         Domain. It must follow the pattern:
+        Domain. It follows the pattern:
 
-         1. It MUST start with a combination of alpha (CI) OR number.
-         2. It MAY be followed by the characters '-'.
-         3. It MUST end with a combination of alpha (CI) with AT LEAST 2 characters.
-         */
+        1. It MUST start with a combination of alpha (CI) OR number.
+        2. It MAY be followed by the characters '-'.
+        3. It MUST end with a combination of alpha (CI) with AT LEAST 2 characters.
+        */
         const val DOMAIN = "[a-zA-Z0-9-]+\\.[a-zA-Z]{2,}"
 
         /*
-         Domain with subdomains. It must follow the pattern:
+        Domain with subdomains. It follows the pattern:
 
-         1. It MUST start with a combination of alpha (CI) OR number.
-         2. It MAY be followed by the characters '.', '-'.
-         3. It MUST end with a combination of alpha (CI) with AT LEAST 2 characters.
-         */
+        1. It MUST start with a combination of alpha (CI) OR number.
+        2. It MAY be followed by the characters '.', '-'.
+        3. It MUST end with a combination of alpha (CI) with AT LEAST 2 characters.
+        */
         const val DOMAIN_SUBDOMAIN = "[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}"
 
         /*
-         URL. It must follow the pattern:
+        URL. It follows the pattern:
 
-         1. It MUST start with 'http' OR 'https'.
-         2. It MUST end with @see *DOMAIN_SUBDOMAIN*.
-         */
+        1. It MUST start with 'http://' OR 'https://'.
+        2. It MUST end with @see *DOMAIN_SUBDOMAIN*.
+        */
         const val URL = "https?:\\/\\/$DOMAIN_SUBDOMAIN"
 
         /*
-         Email address. It must follow the pattern:
+        Email address. It follows the pattern:
 
-         1. It MUST start with a combination of alpha (CI) OR number OR the characters '.', '_', '+', '-'.
-         2. It MUST be followed by the character '@'.
-         3. It MUST end with @see *DOMAIN*.
-         */
+        1. It MUST start with a combination of alpha (CI) OR number OR the characters '.', '_', '+', '-'.
+        2. It MUST be followed by the character '@'.
+        3. It MUST end with @see *DOMAIN*.
+        */
         const val EMAIL = "[a-zA-Z0-9._+-]+@$DOMAIN_SUBDOMAIN"
 
         /*
-         Username. It must follow the pattern:
+        Username. It follows the pattern:
 
-         1. It MUST start with a combination of alpha (CI).
-         2. It MAY be followed by the characters '-', '_'.
-         3. It MUST end with alpha (CI) OR number.
-         */
+        1. It MUST start with a combination of alpha (CI).
+        2. It MAY be followed by the characters '-', '_'.
+        3. It MUST end with alpha (CI) OR number.
+        */
         const val USERNAME = "[a-zA-Z]+(?:[_-]*[a-zA-Z0-9]+)*"
     }
 
